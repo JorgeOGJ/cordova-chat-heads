3f80736d4569c476d5ed96b997a76ada823f4693
Aidan <darrylemoonman@gmail.com>
2017-01-18 15:05:41 +1100

    commit

--- www/ChatHeads.js
+++ www/ChatHeads.js
@@ -6,20 +6,20 @@ var ChatHeads = {};
 ChatHeads.start = function(successCallback, errorCallback){
 	console.log("ChatHeads Start");
 	var args = {};
-	exec(successCallback, errorCallback, "ChatHeads", "startChatHeads", args);
+	cordova.exec(successCallback, errorCallback, "ChatHeads", "startChatHeads", args);
 };
 
 ChatHeads.openHead = function(successCallback, errorCallback){
 	console.log("ChatHeads Open");
 	var args = {};
-	exec(successCallback, errorCallback, "ChatHeads", "openChatHeads", args);
+	cordova.exec(successCallback, errorCallback, "ChatHeads", "openChatHeads", args);
 };
 
 ChatHeads.test = function(successCallback, errorCallback){
 	var testmessage = "This is the test message";
 	console.log("ChatHeads Test");
 	var args = {};
-	exec(successCallback, errorCallback, "ChatHeads", "test", [testmessage]);
+	cordova.exec(successCallback, errorCallback, "ChatHeads", "test", [testmessage]);
 };
 
 module.exports = ChatHeads;
