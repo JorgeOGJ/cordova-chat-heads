96183f3c8cf6ce3b87214d2fe79fce4052102c23
Aidan <darrylemoonman@gmail.com>
2017-01-18 14:25:56 +1100

    commit

--- www/ChatHeads.js
+++ www/ChatHeads.js
@@ -1,1 +1,14 @@
+var argscheck = require('cordova/argscheck'),
+    exec = require('cordova/exec');
+	
+var ChatHeads = {};
 
+ChatHeads.start = function(successCallback, errorCallback){
+	exec(successCallback, errorCallback, "ChatHeads", "startChatHeads", args);
+};
+
+ChatHeads.openHead = function(successCallback, errorCallback){
+	exec(successCallback, errorCallback, "ChatHeads", "openChatHeads", args);
+};
+
+module.exports = ChatHeads;
